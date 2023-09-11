aac94b45657fbb197908c6c63cb7b475a72d549f
maercio <maerciomamedes@hotmail.com>
2023-09-11 15:40:06 -0300

    loop selection menu

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class Main {
 
@@ -6,8 +6,37 @@ public class Main {
         Client client = new Client(1, "maercio");
         Menu menu = new Menu();
         Account account = new Account(111,client.getId(),"conta corrente", 0);
-        menu.line();
+        Scanner scanner = new Scanner(System.in);
+        int option_selected;
 
-        System.out.printf("Cliente criado foi %s, sua conta tem R$ %.2f", client.getName(), account.getBalance());
+
+        do {
+            menu.line();
+            menu.options();
+            option_selected = scanner.nextInt();
+
+            switch (option_selected){
+                case 1:
+                    System.out.printf("Seu saldo é %.2f", account.getBalance());
+                    break;
+                case 2:
+                    System.out.println("dfsdf");
+                    break;
+
+                case 3:
+                    System.out.println("TRANSFERIR");
+                    break;
+
+                case 4:
+                    System.out.println("DEPOSITAR");
+                    break;
+
+                case 0:
+                    System.out.println("ATENDIMENTO ENCERRADO");
+                    break;
+            }
+        } while (option_selected != 0);
+
     }
+
 }
--- src/Menu.java
+++ src/Menu.java
@@ -2,4 +2,17 @@ public class Menu {
     public void line(){
         System.out.println("**************************************");
     }
+
+    public void options(){
+        System.out.println("""
+                ESOLHA A OPÇÃO DESEJADA
+                
+                1 - CONSULTAR SALDO
+                2 - REALIZAR SAQUE
+                3 - TRANSFERIR
+                4 - DEPOSITAR
+                0 - ENCERRAR ATENDIMENTO
+                
+                """);
+    }
 }
